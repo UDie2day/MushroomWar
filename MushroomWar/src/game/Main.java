59e27372c3e610b5496eda77d79b6baab4439ce8
Tykur <Tykur14@msn.com>
2016-05-23 13:59:10 -0400

    Add files via upload

--- MushroomWar/src/game/Main.java
+++ MushroomWar/src/game/Main.java
@@ -1,11 +1,15 @@
 package game;
 
 public class Main {
- //aaaaaaaaaaa
- public int ayy = 0;
+	
+	public int ayy11 = 0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
 	}
 
 }
